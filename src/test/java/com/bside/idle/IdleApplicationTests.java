a63d23c513a99237b29ea69767dda9d9d84c75a7
ghkvud2 <ghkvud2@naver.com>
2022-07-10 19:52:38 +0900

    project init

---
+++ src/test/java/com/bside/idle/IdleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bside.idle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
