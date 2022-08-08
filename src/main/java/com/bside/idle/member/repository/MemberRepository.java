360407f54c3354106a8cb28fce513d6636090d37
ghkvud2 <ghkvud2@naver.com>
2022-08-08 19:29:58 +0900

    fix error

--- src/main/java/com/bside/idle/member/repository/MemberRepository.java
+++ src/main/java/com/bside/idle/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.bside.idle.member.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.bside.idle.entity.Member;
 
@@ -10,9 +12,9 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 	Optional<Member> findByEmail(String email);
 
-	// @Query("select m from Member m "
-	// 	+ "join fetch m.memberCriteria mc "
-	// 	+ "join fetch m.notices "
-	// 	+ "where m.id = :memberId")
-	// Optional<Member> findMemberById(@Param("memberId") Long memberId);
+	@Query("select m from Member m "
+		+ "join fetch m.memberCriteria mc "
+		+ "join fetch m.notices "
+		+ "where m.id = :memberId")
+	Optional<Member> findMemberById(@Param("memberId") Long memberId);
 }
